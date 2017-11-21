4ce50e4c74acf40aab6cd8aafe3bf4ac68bd8bc2
内藤 秀彦 <hidehiko@7110.email>
2017-11-21 23:59:09 +0900

    add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+const browserSync = require('browser-sync');
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const cleanCSS = require('gulp-clean-css');
+const header = require('gulp-header');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+
+
+const namespace = '7110';
+
+const paths = {
+  'html': '**/*.html',
+  'src': {
+    'es6': 'src/es6/**/*.es6',
+    'scss': 'src/scss/**/*.scss',
+  },
+  'dst': {
+    'js': 'dst/js/',
+    'css': 'dst/css/',
+  }
+};
+
+
+/* ====================
+  task modules
+==================== */
+
+// compile scss
+gulp.task('sass', () => {
+  gulp.src(paths.src.scss)
+  .pipe(sourcemaps.init())
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions']
+  }))
+  .pipe(sass({
+    outputStyle: 'expanded'
+  }))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(paths.dst.css))
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(cleanCSS({
+    'compatibility': {
+      'properties': {
+        'colors': false
+      }
+    }
+  }))
+  .pipe(header('/* Copyright 2017 '+namespace+' All Rights Reserved. */\n'))
+  .pipe(gulp.dest(paths.dst.css));
+});
+
+
+// compile es6
+gulp.task('es6', () => {
+  gulp.src(paths.src.es6)
+  .pipe(sourcemaps.init())
+  .pipe(babel())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(paths.dst.js))
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(uglify())
+  .pipe(gulp.dest(paths.dst.js));
+});
+
+
+// browser sync init
+gulp.task('bs-init', () => {
+  browserSync.init({
+    'server': './'
+  });
+});
+
+
+// browser sync reload
+gulp.task('bs-reload', () => {
+  browserSync.reload();
+});
+
+
+/* ====================
+  tasks
+==================== */
+
+// gulp tasks of starting bs-init and compiling sass and es6 + live reload
+gulp.task('default', ['bs-init', 'sass', 'es6'], () => {
+  gulp.watch(paths.html, ['bs-reload']);
+  gulp.watch(paths.src.scss, ['sass', 'bs-reload']);
+  gulp.watch(paths.src.es6, ['es6', 'bs-reload']);
+});
+
+
+// gulp tasks of starting bs-init and compiling sass and es6
+gulp.task('compile', ['bs-init', 'sass', 'es6'], () => {
+  gulp.watch(paths.src.scss, ['sass']);
+  gulp.watch(paths.src.es6, ['es6']);
+});
